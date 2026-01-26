9ec7966c08ce9ff21afca22a82cdf0a9b7e3e5de
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2026-01-26 22:52:26 +0700

    tạo giao diện signup

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { SignupForm } from "../components/signup-form";
 
 const SignUp = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <SignupForm />
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
+
