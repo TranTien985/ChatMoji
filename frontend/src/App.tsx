2fc77574624ef99bd168d348f647a87db6d49656
Tiennk5 <154210654+Tiennk5@users.noreply.github.com>
2026-01-26 21:49:57 +0700

    tạo route cho các trang, cài đặt shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router";
+import {Toaster} from 'sonner';
+import SignIn from "./pages/SigninPage";
+import SignUp from "./pages/SignupPage";
+import ChatApp from "./pages/ChatApp";
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <>
+    <Toaster richColors/>
+    <BrowserRouter>
+      <Routes>
+        {/* public routes */}
+        <Route
+          path="/signin"
+          element={<SignIn/>}
+        />
+        <Route
+          path="/signup"
+          element={<SignUp/>}
+        />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        {/* protected routes */}
+        <Route
+          path="/"
+          element={<ChatApp/>}
+        />
+      </Routes>
+    </BrowserRouter>
+  </>;
 }
 
-export default App
+export default App;
